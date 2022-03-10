9a95edaa44f49beeaaa17486b6e8d0f7f7599a8d
Seph Freer <sephfreer@gmail.com>
2022-03-10 22:13:12 +1100

    Style Conference test site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,20 @@ class PagesController < ApplicationController
   def home
     
   end
+  def speakers
+    #@start_date = Time.zone.local(*params[:input_dob].sort.map(&:last).map(&:to_i))
+    # @start_date =  [params["input[dob(1i)]"].to_i,
+    # params["input[dob(2i)]"].to_i,
+    # params["input[dob(3i)]"].to_i]  #Date.new() -> []    
+    @start_date = params.to_enum.to_h
+  end
+  def venue
+
+  end
+  def schedule
+
+  end
+  def register
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root 'application#hello'  # application_controller.rb
   root 'pages#home'  # pages_controller.rb#home
+  get '/speakers', to: 'pages#speakers', as: :speakers
+  get '/venue', to: 'pages#venue', as: :venue
+  get '/schedule', to: 'pages#schedule', as: :schedule
+  get '/register', to: 'pages#register', as: :register
 end
